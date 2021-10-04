f08a87a8091af65af2c79d2827d4cdb222fd853c
smurphy7326 <smurphy7326@gmail.com>
2021-10-03 23:40:06 -0400

    trying

--- server.js
+++ server.js
@@ -42,15 +42,19 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
   }
 
+  function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
 
-  app.get('/api/animals', (req, res) => {
-    let results = animals;
-    if (req.query) {
-      results = filterByQuery(req.query, results);
+  app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
     }
-    res.json(results);
   });
-  
   
 
   app.listen(PORT, () => {
